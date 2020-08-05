0ac46852d344c89c1916a7edfff9a386caa177be
thudsonbu <hudson_thom@bentley.edu>
2020-08-04 21:03:31 -0600

    Todolist from and connected to redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
 import Todo from './Todo'
+import { connect } from 'react-redux'
 
 class TodoList extends Component {
     constructor(props){
         super(props)
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            task: ""
+        }
     }
-    
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.dispatch({})
+    }
+    handleChange(e){
+        this.setState({
+            [e.target.name]: [e.target.value]
+        })
+    }
     render(){
         // create our todos using state
-        let todos = this.state.todos.map((todo, index) => <Todo todo={todo} key={index} />)
+        let todos = this.props.todos.map((todo, index) => 
+            (<Todo todo={todo} key={index} />)
+        )
+
         return(
             <div className="Todos">
+                <form onSubmit = {this.handleSubmit}>
+                    <label htmlFor="task"> Task </label>
+                    <input type="text" name="task" id="task" onChange={this.handleChange}/>
+                    <button>Button</button>
+                </form>
                 <ul>
                     {todos}
                 </ul>
@@ -19,4 +41,10 @@ class TodoList extends Component {
     }
 }
 
-export default TodoList
+function mapStateToProps(reduxState){
+    return {
+        todos: reduxState.todos
+    }
+}
+
+export default connect(mapStateToProps)(TodoList)
